9f34971e4546c2eb18ad0c07375d42072f813475
Vladimir Nachev <vladimir.nachev@sap.com>
2020-11-30 21:26:29 +0200

    Update cloud credentials even for unwanted machine deployments.

--- pkg/controller/worker/machine_controller_manager.go
+++ pkg/controller/worker/machine_controller_manager.go
@@ -77,3 +77,9 @@ func (w *workerDelegate) GetMachineControllerManagerShootChartValues(ctx context
 		"providerName": openstack.Name,
 	}, nil
 }
+
+// GetMachineControllerManagerCloudCredentials should return the IaaS credentials
+// with the secret keys used by the machine-controller-manager.
+func (w *workerDelegate) GetMachineControllerManagerCloudCredentials(ctx context.Context) (map[string][]byte, error) {
+	return w.generateMachineClassSecretData(ctx)
+}

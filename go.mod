01315db573373f1e388b2ea8524f8817a554ca8a
ialidzhikov <i.alidjikov@gmail.com>
2021-08-21 21:13:04 +0300

    Vendor gardener/gardener@v1.29.0
    Signed-off-by: ialidzhikov <i.alidjikov@gmail.com>

--- go.mod
+++ go.mod
@@ -3,13 +3,12 @@ module github.com/gardener/gardener-extension-provider-openstack
 go 1.16
 
 require (
-	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/sprig v2.22.0+incompatible
 	github.com/ahmetb/gen-crd-api-reference-docs v0.2.0
 	github.com/coreos/go-systemd/v22 v22.1.0
 	github.com/frankban/quicktest v1.9.0 // indirect
 	github.com/gardener/etcd-druid v0.5.0
-	github.com/gardener/gardener v1.26.1-0.20210712101613-e8eb807d790f
+	github.com/gardener/gardener v1.29.0
 	github.com/gardener/machine-controller-manager v0.36.0
 	github.com/go-logr/logr v0.4.0
 	github.com/golang/mock v1.6.0
@@ -17,7 +16,6 @@ require (
 	github.com/google/uuid v1.1.2
 	github.com/gophercloud/gophercloud v0.7.0
 	github.com/gophercloud/utils v0.0.0-20200204043447-9864b6f1f12f
-	github.com/mitchellh/copystructure v1.1.1 // indirect
 	github.com/nwaples/rardecode v1.1.0 // indirect
 	github.com/onsi/ginkgo v1.16.4
 	github.com/onsi/gomega v1.13.0
@@ -43,6 +41,7 @@ require (
 )
 
 replace (
+	github.com/gardener/gardener-resource-manager/api => github.com/gardener/gardener-resource-manager/api v0.25.0
 	github.com/prometheus/client_golang => github.com/prometheus/client_golang v1.11.0 // keep this value in sync with sigs.k8s.io/controller-runtime
 	k8s.io/api => k8s.io/api v0.21.2
 	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.21.2
--- vendor/github.com/gardener/gardener/pkg/utils/kubernetes/kubernetes.go
+++ vendor/github.com/gardener/gardener/pkg/utils/kubernetes/kubernetes.go
@@ -188,9 +188,9 @@ func WaitUntilLoadBalancerIsReady(ctx context.Context, kubeClient kubernetes.Int
 	if err := retry.UntilTimeout(ctx, 5*time.Second, timeout, func(ctx context.Context) (done bool, err error) {
 		loadBalancerIngress, err = GetLoadBalancerIngress(ctx, kubeClient.Client(), service)
 		if err != nil {
-			logger.Infof("Waiting until the %s service deployed is ready...", name)
+			logger.Infof("Waiting until the %s service is ready...", name)
 			// TODO(AC): This is a quite optimistic check / we should differentiate here
-			return retry.MinorError(fmt.Errorf("%s service deployed is not ready: %v", name, err))
+			return retry.MinorError(fmt.Errorf("%s service is not ready: %v", name, err))
 		}
 		return retry.Ok()
 	}); err != nil {
@@ -344,7 +344,7 @@ func FetchEventMessages(ctx context.Context, scheme *runtime.Scheme, reader clie
 	}
 	eventList := &corev1.EventList{}
 	if err := reader.List(ctx, eventList, fieldSelector); err != nil {
-		return "", fmt.Errorf("error '%v' occurred while fetching more details", err)
+		return "", fmt.Errorf("error '%w' occurred while fetching more details", err)
 	}
 
 	if len(eventList.Items) > 0 {
@@ -533,7 +533,7 @@ func NewestPodForDeployment(ctx context.Context, c client.Reader, deployment *ap
 
 	podSelector, err := metav1.LabelSelectorAsSelector(newestReplicaSet.Spec.Selector)
 	if err != nil {
-		return nil, fmt.Errorf("failed to convert the pod selector from ReplicaSet %s/%s: %v", newestReplicaSet.Namespace, newestReplicaSet.Name, err)
+		return nil, fmt.Errorf("failed to convert the pod selector from ReplicaSet %s/%s: %w", newestReplicaSet.Namespace, newestReplicaSet.Name, err)
 	}
 
 	listOpts = append(listOpts, client.MatchingLabelsSelector{Selector: podSelector})

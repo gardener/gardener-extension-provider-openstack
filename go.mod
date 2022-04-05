12f49534922f017d8a9611d305c41544bfd93ee0
Tim Ebert <timebertt@gmail.com>
2022-04-05 09:58:14 +0200

    [automated] make revendor

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/Masterminds/sprig v2.22.0+incompatible
 	github.com/ahmetb/gen-crd-api-reference-docs v0.2.0
 	github.com/coreos/go-systemd/v22 v22.3.2
-	github.com/gardener/etcd-druid v0.7.0
+	github.com/gardener/etcd-druid v0.8.0
 	github.com/gardener/gardener v1.44.0
 	github.com/gardener/machine-controller-manager v0.41.0
 	github.com/go-logr/logr v1.2.0
@@ -25,7 +25,7 @@ require (
 	k8s.io/apiextensions-apiserver v0.23.3
 	k8s.io/apimachinery v0.23.3
 	k8s.io/apiserver v0.23.3
-	k8s.io/autoscaler/vertical-pod-autoscaler v0.0.0-00010101000000-000000000000
+	k8s.io/autoscaler/vertical-pod-autoscaler v0.9.0
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
 	k8s.io/code-generator v0.23.3
 	k8s.io/component-base v0.23.3
--- vendor/k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1/doc.go
+++ vendor/k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1/doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 The Kubernetes Authors.
+Copyright 2019 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,15 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package builder wraps other controller-runtime libraries and exposes simple
-// patterns for building common Controllers.
-//
-// Projects built with the builder package can trivially be rebased on top of the underlying
-// packages if the project requires more customized behavior in the future.
-package builder
+// +k8s:deepcopy-gen=package,register
 
-import (
-	logf "sigs.k8s.io/controller-runtime/pkg/internal/log"
-)
-
-var log = logf.RuntimeLog.WithName("builder")
+// Package v1 contains definitions of Vertical Pod Autoscaler related objects.
+// +groupName=autoscaling.k8s.io
+package v1

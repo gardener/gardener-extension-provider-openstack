db556adc9e0aa641669b2b99865194a0f88ebe64
Rafael Franzke <rafael.franzke@sap.com>
2022-04-29 13:37:37 +0200

    Cleanup `useTokenRequestor` and `useProjectedTokenMount` flags in worker controller

--- pkg/controller/worker/actuator.go
+++ pkg/controller/worker/actuator.go
@@ -43,7 +43,7 @@ type delegateFactory struct {
 }
 
 // NewActuator creates a new Actuator that updates the status of the handled WorkerPoolConfigs.
-func NewActuator(useTokenRequestor, useProjectedTokenMount bool) worker.Actuator {
+func NewActuator() worker.Actuator {
 	delegateFactory := &delegateFactory{
 		logger: log.Log.WithName("worker-actuator"),
 	}
@@ -56,8 +56,6 @@ func NewActuator(useTokenRequestor, useProjectedTokenMount bool) worker.Actuator
 		mcmShootChart,
 		imagevector.ImageVector(),
 		extensionscontroller.ChartRendererFactoryFunc(util.NewChartRendererForShoot),
-		useTokenRequestor,
-		useProjectedTokenMount,
 	)
 }
 

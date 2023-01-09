5cc7d824e5da5ccc48e4f572e786515c59e26cae
Felix Breuer <fbreuer@pm.me>
2023-01-09 11:19:33 +0100

    attach external network to router in infrastructure test
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

--- test/integration/infrastructure/infrastructure_test.go
+++ test/integration/infrastructure/infrastructure_test.go
@@ -66,6 +66,8 @@ var (
 	region           = flag.String("region", "", "Openstack region")
 	tenantName       = flag.String("tenant-name", "", "Tenant name for openstack")
 	userName         = flag.String("user-name", "", "User name for openstack")
+
+	floatingPoolID string
 )
 
 func validateFlags() {
@@ -177,6 +179,16 @@ var _ = BeforeSuite(func() {
 
 	openstackClient, err = NewOpenstackClient(*authURL, *domainName, *floatingPoolName, *password, *region, *tenantName, *userName)
 	Expect(err).NotTo(HaveOccurred())
+
+	// Retrieve FloatingPoolNetworkID
+	page, err := networks.List(openstackClient.NetworkingClient, networks.ListOpts{
+		Name: *floatingPoolName,
+	}).AllPages()
+	Expect(err).NotTo(HaveOccurred())
+	networkList, err := networks.ExtractNetworks(page)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(networkList).To(HaveLen(1))
+	floatingPoolID = networkList[0].ID
 
 	priorityClass := &schedulingv1.PriorityClass{
 		ObjectMeta: metav1.ObjectMeta{
@@ -551,6 +563,9 @@ func prepareNewRouter(ctx context.Context, log logr.Logger, routerName string, o
 
 	createOpts := routers.CreateOpts{
 		Name: routerName,
+		GatewayInfo: &routers.GatewayInfo{
+			NetworkID: floatingPoolID,
+		},
 	}
 	router, err := routers.Create(openstackClient.NetworkingClient, createOpts).Extract()
 	Expect(err).NotTo(HaveOccurred())

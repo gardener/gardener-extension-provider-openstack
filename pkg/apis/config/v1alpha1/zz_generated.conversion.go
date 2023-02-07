4438043d3e0d6db2fa03fa80b213526a8516ba6f
Shafeeque E S <shafeeque.e.s@sap.com>
2023-02-07 12:50:12 +0530

    Run `make generate`

--- pkg/apis/config/v1alpha1/zz_generated.conversion.go
+++ pkg/apis/config/v1alpha1/zz_generated.conversion.go
@@ -24,7 +24,6 @@ package v1alpha1
 import (
 	unsafe "unsafe"
 
-	config "github.com/gardener/gardener-extension-provider-openstack/pkg/apis/config"
 	apisconfig "github.com/gardener/gardener/extensions/pkg/apis/config"
 	apisconfigv1alpha1 "github.com/gardener/gardener/extensions/pkg/apis/config/v1alpha1"
 	resource "k8s.io/apimachinery/pkg/api/resource"
@@ -32,6 +31,8 @@ import (
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	componentbaseconfig "k8s.io/component-base/config"
 	configv1alpha1 "k8s.io/component-base/config/v1alpha1"
+
+	config "github.com/gardener/gardener-extension-provider-openstack/pkg/apis/config"
 )
 
 func init() {

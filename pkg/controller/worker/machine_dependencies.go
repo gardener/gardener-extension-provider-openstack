6e690127c3a539b1670bc79b9708aba5ccbc8805
ialidzhikov <i.alidjikov@gmail.com>
2023-03-13 10:42:28 +0200

    Switch to the non-deprecated funcs of the WorkerDelegate
    The `DeployMachineDependencies` and `CleanupMachineDependencies` funcs were deprecated with https://github.com/gardener/gardener/pull/6290.

--- pkg/controller/worker/machine_dependencies.go
+++ pkg/controller/worker/machine_dependencies.go
@@ -28,7 +28,20 @@ import (
 	osclient "github.com/gardener/gardener-extension-provider-openstack/pkg/openstack/client"
 )
 
-func (w *workerDelegate) DeployMachineDependencies(ctx context.Context) error {
+// DeployMachineDependencies implements genericactuator.WorkerDelegate.
+// Deprecated: Do not use this func. It is deprecated in genericactuator.WorkerDelegate.
+func (w *workerDelegate) DeployMachineDependencies(_ context.Context) error {
+	return nil
+}
+
+// CleanupMachineDependencies implements genericactuator.WorkerDelegate.
+// Deprecated: Do not use this func. It is deprecated in genericactuator.WorkerDelegate.
+func (w *workerDelegate) CleanupMachineDependencies(_ context.Context) error {
+	return nil
+}
+
+// PreReconcileHook implements genericactuator.WorkerDelegate.
+func (w *workerDelegate) PreReconcileHook(ctx context.Context) error {
 	computeClient, err := w.openstackClient.Compute()
 	if err != nil {
 		return err
@@ -95,7 +108,29 @@ func (w *workerDelegate) reconcilePoolServerGroup(computeClient osclient.Compute
 	}, nil
 }
 
-func (w *workerDelegate) CleanupMachineDependencies(ctx context.Context) error {
+// PostReconcileHook implements genericactuator.WorkerDelegate.
+func (w *workerDelegate) PostReconcileHook(ctx context.Context) error {
+	return w.cleanupMachineDependencies(ctx)
+}
+
+// PreDeleteHook implements genericactuator.WorkerDelegate.
+func (w *workerDelegate) PreDeleteHook(_ context.Context) error {
+	return nil
+}
+
+// PostDeleteHook implements genericactuator.WorkerDelegate.
+func (w *workerDelegate) PostDeleteHook(ctx context.Context) error {
+	return w.cleanupMachineDependencies(ctx)
+}
+
+// cleanupMachineDependencies cleans up machine dependencies.
+//
+// TODO(dkistner, kon-angelo): Currently both PostReconcileHook and PostDeleteHook funcs call cleanupMachineDependencies.
+// cleanupMachineDependencies calls cleanupServerGroupDependencies. cleanupServerGroupDependencies handles the cases when the Worker is being
+// deleted (logic applicable for PostDeleteHook) and is not being deleted (logic applicable for PostReconcileHook).
+// Refactor this so that PostDeleteHook executes only the handling for Worker being deleted and PostReconcileHook executes only
+// the handling for Worker reconciled (not being deleted).
+func (w *workerDelegate) cleanupMachineDependencies(ctx context.Context) error {
 	computeClient, err := w.openstackClient.Compute()
 	if err != nil {
 		return err
@@ -179,24 +214,4 @@ func (w *workerDelegate) cleanupServerGroupDependencies(computeClient osclient.C
 		set.deleteByPoolName(d.PoolName)
 		return nil
 	})
-}
-
-// PreReconcileHook implements genericactuator.WorkerDelegate.
-func (w *workerDelegate) PreReconcileHook(_ context.Context) error {
-	return nil
-}
-
-// PostReconcileHook implements genericactuator.WorkerDelegate.
-func (w *workerDelegate) PostReconcileHook(_ context.Context) error {
-	return nil
-}
-
-// PreDeleteHook implements genericactuator.WorkerDelegate.
-func (w *workerDelegate) PreDeleteHook(_ context.Context) error {
-	return nil
-}
-
-// PostDeleteHook implements genericactuator.WorkerDelegate.
-func (w *workerDelegate) PostDeleteHook(_ context.Context) error {
-	return nil
 }

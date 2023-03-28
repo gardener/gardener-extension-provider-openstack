02c88f144b4c0f38d9cb6b05a368d525df547e69
Martin Weindel <martin.weindel@sap.com>
2023-03-28 13:24:09 +0200

    Add machine labels to worker pool (#579)
    * add machine labels to worker pool and include them optionally to workerpool hash
    
    * changed  to , more conservative hashcode calculation

--- pkg/controller/worker/machines_test.go
+++ pkg/controller/worker/machines_test.go
@@ -754,6 +754,114 @@ var _ = Describe("Machines", func() {
 					Expect(err).To(HaveOccurred())
 					Expect(err.Error()).To(Equal(`server group is required for pool "pool-1", but no server group dependency found`))
 				})
+
+				It("should consider rolling machine labels for the worker pool hash", func() {
+					setup(region, machineImage, "")
+
+					applyLabelsAndPolicy := func(labels []apiv1alpha1.MachineLabel, policy *string) string {
+						w.Spec.Pools[0].Labels = map[string]string{"k1": "v1"}
+						workerConfig := &apiv1alpha1.WorkerConfig{
+							TypeMeta: metav1.TypeMeta{
+								Kind:       "WorkerConfig",
+								APIVersion: apiv1alpha1.SchemeGroupVersion.String(),
+							},
+							MachineLabels: labels,
+						}
+						if policy != nil {
+							workerConfig.ServerGroup = &apiv1alpha1.ServerGroup{Policy: *policy}
+							w.Status.ProviderStatus = &runtime.RawExtension{
+								Object: &apiv1alpha1.WorkerStatus{
+									TypeMeta: metav1.TypeMeta{
+										Kind:       "WorkerStatus",
+										APIVersion: apiv1alpha1.SchemeGroupVersion.String(),
+									},
+									ServerGroupDependencies: []apiv1alpha1.ServerGroupDependency{
+										{
+											PoolName: namePool1,
+											Name:     "servergroup1",
+											ID:       "id1",
+										},
+									},
+								},
+							}
+						}
+						w.Spec.Pools[0].ProviderConfig = &runtime.RawExtension{
+							Raw: encode(workerConfig),
+						}
+						workerDelegate, _ := NewWorkerDelegate(common.NewClientContext(c, scheme, decoder), chartApplier, "", w, cluster, nil)
+						result, err := workerDelegate.GenerateMachineDeployments(context.TODO())
+						Expect(err).NotTo(HaveOccurred())
+						Expect(result[0].Labels).To(Equal(map[string]string{"k1": "v1"}))
+						return result[0].ClassName
+					}
+
+					className0 := applyLabelsAndPolicy(nil, nil)
+					className1 := applyLabelsAndPolicy([]apiv1alpha1.MachineLabel{
+						{Name: "foo", Value: "bar"},
+					}, nil)
+					className1b := applyLabelsAndPolicy([]apiv1alpha1.MachineLabel{
+						{Name: "foo", Value: "bar2"},
+					}, nil)
+					className2 := applyLabelsAndPolicy([]apiv1alpha1.MachineLabel{
+						{Name: "foo", Value: "bar"},
+						{Name: "vmspec/a", Value: "blabla", TriggerRollingOnUpdate: true},
+						{Name: "vmspec/c", Value: "rabarber1", TriggerRollingOnUpdate: true},
+					}, nil)
+					className2b := applyLabelsAndPolicy([]apiv1alpha1.MachineLabel{
+						{Name: "vmspec/c", Value: "rabarber1", TriggerRollingOnUpdate: true},
+						{Name: "vmspec/b", Value: "abc"},
+						{Name: "vmspec/a", Value: "blabla", TriggerRollingOnUpdate: true},
+					}, nil)
+					className3 := applyLabelsAndPolicy([]apiv1alpha1.MachineLabel{
+						{Name: "foo", Value: "bar"},
+						{Name: "vmspec/a", Value: "blabla", TriggerRollingOnUpdate: true},
+						{Name: "vmspec/c", Value: "rabarber2", TriggerRollingOnUpdate: true},
+					}, nil)
+					className4 := applyLabelsAndPolicy([]apiv1alpha1.MachineLabel{
+						{Name: "foo", Value: "bar"},
+						{Name: "vmspec/a", Value: "blabla", TriggerRollingOnUpdate: true},
+						{Name: "vmspec/c", Value: "rabarber2", TriggerRollingOnUpdate: false},
+					}, nil)
+
+					Expect(className0).NotTo(Equal(className1)) // should be equal, but cannot be avoided as old and new hash mechanism are colliding
+					Expect(className1).To(Equal(className1b))
+					Expect(className0).NotTo(Equal(className2))
+					Expect(className2).To(Equal(className2b))
+					Expect(className0).NotTo(Equal(className3))
+					Expect(className2).NotTo(Equal(className3))
+					Expect(className3).NotTo(Equal(className4))
+
+					By("with server group policy")
+					policy1 := pointer.String("soft-anti-affinity")
+					policy2 := pointer.String("foo")
+					classNamePolicy01 := applyLabelsAndPolicy(nil, policy1)
+					classNamePolicy02 := applyLabelsAndPolicy(nil, policy2)
+					classNamePolicy11 := applyLabelsAndPolicy([]apiv1alpha1.MachineLabel{
+						{Name: "foo", Value: "bar"},
+					}, policy1)
+					classNamePolicy21 := applyLabelsAndPolicy([]apiv1alpha1.MachineLabel{
+						{Name: "foo", Value: "bar"},
+						{Name: "vmspec/a", Value: "blabla", TriggerRollingOnUpdate: true},
+						{Name: "vmspec/c", Value: "rabarber1", TriggerRollingOnUpdate: true},
+					}, policy1)
+					classNamePolicy22 := applyLabelsAndPolicy([]apiv1alpha1.MachineLabel{
+						{Name: "foo", Value: "bar"},
+						{Name: "vmspec/a", Value: "blabla", TriggerRollingOnUpdate: true},
+						{Name: "vmspec/c", Value: "rabarber1", TriggerRollingOnUpdate: true},
+					}, policy2)
+					classNamePolicy22b := applyLabelsAndPolicy([]apiv1alpha1.MachineLabel{
+						{Name: "vmspec/a", Value: "blabla", TriggerRollingOnUpdate: true},
+						{Name: "vmspec/c", Value: "rabarber1", TriggerRollingOnUpdate: true},
+					}, policy2)
+
+					Expect(className0).NotTo(Equal(classNamePolicy01))
+					Expect(className0).NotTo(Equal(classNamePolicy02))
+					Expect(classNamePolicy01).NotTo(Equal(classNamePolicy02))
+					Expect(classNamePolicy01).NotTo(Equal(classNamePolicy11)) // should be equal, but cannot be avoided as old and new hash mechanism are colliding
+					Expect(classNamePolicy11).NotTo(Equal(classNamePolicy21))
+					Expect(classNamePolicy21).NotTo(Equal(classNamePolicy22))
+					Expect(classNamePolicy22).To(Equal(classNamePolicy22b))
+				})
 			})
 
 			It("should fail because the version is invalid", func() {

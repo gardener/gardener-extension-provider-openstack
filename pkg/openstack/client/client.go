d9faed13dabcbc500c9efe3909e824daa0233ed7
Jan Lohage <j2L4e@users.noreply.github.com>
2023-09-21 12:38:25 +0200

    Honor cloudprofile specific TLS settings (#668)
    * use NewClient instead of AuthenticatedClient to be able to inject HTTPClient
    
    * Remove obsolete function call
    
    ---------
    
    Co-authored-by: Lothar Bach <lothar@crml.de>

--- pkg/openstack/client/client.go
+++ pkg/openstack/client/client.go
@@ -81,7 +81,14 @@ func NewOpenstackClientFromCredentials(credentials *os.Credentials) (Factory, er
 	// re-authenticate automatically if/when your token expires.
 	authOpts.AllowReauth = true
 
-	provider, err := openstack.AuthenticatedClient(*authOpts)
+	provider, err := openstack.NewClient(authOpts.IdentityEndpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	provider.HTTPClient = *opts.HTTPClient
+
+	err = openstack.Authenticate(provider, *authOpts)
 	if err != nil {
 		return nil, err
 	}

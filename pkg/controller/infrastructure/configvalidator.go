97fd4d28c018f6d5ef692ed0696ae153549c9e4f
Kai Kummerer <kai.kummerer@stackit.de>
2023-11-03 17:37:42 +0100

    extend configValidator

--- pkg/controller/infrastructure/configvalidator.go
+++ pkg/controller/infrastructure/configvalidator.go
@@ -12,6 +12,9 @@ import (
 	"github.com/gardener/gardener/extensions/pkg/controller/infrastructure"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	"github.com/go-logr/logr"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/layer3/routers"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/networks"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/subnets"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
@@ -70,6 +73,15 @@ func (c *configValidator) Validate(ctx context.Context, infra *extensionsv1alpha
 	// Validate infrastructure config
 	logger.Info("Validating infrastructure configuration")
 	allErrs = append(allErrs, c.validateFloatingPoolName(ctx, networkingClient, config.FloatingPoolName, field.NewPath("floatingPoolName"))...)
+	if config.Networks.ID != nil {
+		allErrs = append(allErrs, c.validateNetwork(ctx, networkingClient, *config.Networks.ID, field.NewPath("networks.id"))...)
+	}
+	if config.Networks.SubnetID != nil {
+		allErrs = append(allErrs, c.validateSubnet(ctx, networkingClient, *config.Networks.SubnetID, *config.Networks.ID, field.NewPath("networks.subnetId"))...)
+	}
+	if config.Networks.Router != nil && config.Networks.Router.ID != "" {
+		allErrs = append(allErrs, c.validateRouter(ctx, networkingClient, config.Networks.Router.ID, field.NewPath("networks.router.id"))...)
+	}
 
 	return allErrs
 }
@@ -89,5 +101,57 @@ func (c *configValidator) validateFloatingPoolName(ctx context.Context, networki
 		allErrs = append(allErrs, field.NotFound(fldPath, floatingPoolName))
 	}
 
+	return allErrs
+}
+
+func (c *configValidator) validateNetwork(ctx context.Context, networkingClient openstackclient.Networking, networkID string, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+
+	networks, err := networkingClient.ListNetwork(networks.ListOpts{ID: networkID})
+	if err != nil {
+		allErrs = append(allErrs, field.InternalError(fldPath, fmt.Errorf("could not get network: %w", err)))
+		return allErrs
+	}
+	if len(networks) == 0 {
+		allErrs = append(allErrs, field.NotFound(fldPath, networkID))
+		return allErrs
+	}
+	return allErrs
+}
+
+func (c *configValidator) validateSubnet(ctx context.Context, networkingClient openstackclient.Networking, subnetID, networkID string, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+
+	// validate subnet existence
+	subnets, err := networkingClient.ListSubnets(subnets.ListOpts{ID: subnetID})
+	if err != nil {
+		allErrs = append(allErrs, field.InternalError(fldPath, fmt.Errorf("could not get subnet: %w", err)))
+		return allErrs
+	}
+	if len(subnets) == 0 {
+		allErrs = append(allErrs, field.NotFound(fldPath, subnetID))
+		return allErrs
+	}
+
+	// validate subnet is in defined network
+	if subnets[0].NetworkID != networkID {
+		allErrs = append(allErrs, field.Invalid(fldPath, subnetID, fmt.Sprintf("specified subnet isn't a child of the specified network %q", networkID)))
+	}
+
+	return allErrs
+}
+
+func (c *configValidator) validateRouter(ctx context.Context, networkingClient openstackclient.Networking, routerID string, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+
+	routers, err := networkingClient.ListRouters(routers.ListOpts{ID: routerID})
+	if err != nil {
+		allErrs = append(allErrs, field.InternalError(fldPath, fmt.Errorf("could not get subnet: %w", err)))
+		return allErrs
+	}
+	if len(routers) == 0 {
+		allErrs = append(allErrs, field.NotFound(fldPath, routerID))
+		return allErrs
+	}
 	return allErrs
 }

2b62fb61db8631ae6903cbd6f58fa4c23d37464f
Tim Usner <tim.usner@sap.com>
2024-01-03 18:21:52 +0100

    Adjust paths after vendor removal

--- pkg/apis/openstack/v1alpha1/doc.go
+++ pkg/apis/openstack/v1alpha1/doc.go
@@ -17,7 +17,7 @@
 // +k8s:openapi-gen=true
 // +k8s:defaulter-gen=TypeMeta
 
-//go:generate gen-crd-api-reference-docs -api-dir . -config ../../../../hack/api-reference/api.json -template-dir ../../../../vendor/github.com/gardener/gardener/hack/api-reference/template -out-file ../../../../hack/api-reference/api.md
+//go:generate gen-crd-api-reference-docs -api-dir . -config ../../../../hack/api-reference/api.json -template-dir $GARDENER_HACK_DIR/api-reference/template -out-file ../../../../hack/api-reference/api.md
 
 // Package v1alpha1 contains the OpenStack provider API resources.
 // +groupName=openstack.provider.extensions.gardener.cloud

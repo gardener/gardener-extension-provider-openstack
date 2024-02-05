f1f4ac60fb3e33d61cba6fc73f318562821c5a41
Konstantinos Angelopoulos <konstantinos.angelopoulos@sap.com>
2024-02-05 10:20:06 +0100

    fix mock targets

--- pkg/openstack/client/types.go
+++ pkg/openstack/client/types.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate mockgen -destination=mocks/client_mocks.go -package=mocks . Factory,FactoryFactory,Compute,DNS,Networking,Loadbalancing
+//go:generate mockgen -destination=mocks/client_mocks.go -package=mocks . Factory,FactoryFactory,Compute,DNS,Networking,Loadbalancing,SharedFilesystem
 package client
 
 import (

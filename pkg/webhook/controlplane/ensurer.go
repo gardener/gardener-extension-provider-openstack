86eb88ac8201ae08129f674ebe8331298ec10bac
acumino <sksgkpvks@gmail.com>
2024-03-09 00:39:13 +0530

    Vendor gardener/gardener@v1.90

--- pkg/webhook/controlplane/ensurer.go
+++ pkg/webhook/controlplane/ensurer.go
@@ -16,7 +16,7 @@ import (
 	"github.com/gardener/gardener/extensions/pkg/webhook/controlplane/genericmutator"
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
-	"github.com/gardener/gardener/pkg/component/machinecontrollermanager"
+	"github.com/gardener/gardener/pkg/component/nodemanagement/machinecontrollermanager"
 	gutil "github.com/gardener/gardener/pkg/utils/gardener"
 	versionutils "github.com/gardener/gardener/pkg/utils/version"
 	"github.com/go-logr/logr"

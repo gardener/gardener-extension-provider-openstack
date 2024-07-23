d9c4f0eefb3970102c66ae94a5bbcbb51aace93c
Duciwuci <duciwuci@gmail.com>
2024-07-23 16:34:33 +0200

    replace empty list with nil when passing vars

--- pkg/controller/worker/machines_test.go
+++ pkg/controller/worker/machines_test.go
@@ -358,8 +358,8 @@ var _ = Describe("Machines", func() {
 					},
 				}
 
-				workerPoolHash1, _ = worker.WorkerPoolHash(w.Spec.Pools[0], cluster, []string{}, []string{})
-				workerPoolHash2, _ = worker.WorkerPoolHash(w.Spec.Pools[1], cluster, []string{}, []string{})
+				workerPoolHash1, _ = worker.WorkerPoolHash(w.Spec.Pools[0], cluster, nil, nil)
+				workerPoolHash2, _ = worker.WorkerPoolHash(w.Spec.Pools[1], cluster, nil, nil)
 
 				workerDelegate, _ = NewWorkerDelegate(c, scheme, chartApplier, "", w, clusterWithoutImages, nil)
 			})

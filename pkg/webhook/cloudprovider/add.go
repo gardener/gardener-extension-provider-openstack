bbb49a47b702d266fa7823899d005e350eae5a35
Duciwuci <duciwuci@gmail.com>
2024-07-23 15:18:40 +0200

    bump Gardener to v1.98 and pass params for WorkerPoolHash method

--- pkg/webhook/cloudprovider/add.go
+++ pkg/webhook/cloudprovider/add.go
@@ -5,8 +5,6 @@
 package cloudprovider
 
 import (
-	"fmt"
-
 	"github.com/Masterminds/semver/v3"
 	extensionswebhook "github.com/gardener/gardener/extensions/pkg/webhook"
 	"github.com/gardener/gardener/extensions/pkg/webhook/cloudprovider"
@@ -31,21 +29,10 @@ func init() {
 // AddToManager creates the cloudprovider webhook and adds it to the manager.
 func AddToManager(gardenerVersion *string) func(mgr manager.Manager) (*extensionswebhook.Webhook, error) {
 	return func(mgr manager.Manager) (*extensionswebhook.Webhook, error) {
-		enable := false
-		if gardenerVersion != nil && len(*gardenerVersion) > 0 {
-			version, err := semver.NewVersion(*gardenerVersion)
-			if err != nil {
-				return nil, fmt.Errorf("failed to parse gardener version: %v", err)
-			}
-			if versionConstraintGreaterEqual142.Check(version) {
-				enable = true
-			}
-		}
 		logger.Info("adding webhook to manager")
 		return cloudprovider.New(mgr, cloudprovider.Args{
-			Provider:             openstack.Type,
-			Mutator:              cloudprovider.NewMutator(mgr, logger, NewEnsurer(mgr, logger)),
-			EnableObjectSelector: enable,
+			Provider: openstack.Type,
+			Mutator:  cloudprovider.NewMutator(mgr, logger, NewEnsurer(mgr, logger)),
 		})
 	}
 }

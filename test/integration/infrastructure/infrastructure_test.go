e1b54805884829bb8203a18c48360679d104d4de
Konstantinos Angelopoulos <konstantinos.angelopoulos@sap.com>
2024-10-04 13:45:05 +0200

    parallel tests

--- test/integration/infrastructure/infrastructure_test.go
+++ test/integration/infrastructure/infrastructure_test.go
@@ -29,10 +29,16 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	schedulingv1 "k8s.io/api/scheduling/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/apimachinery/pkg/util/uuid"
+	schemev1 "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
 	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -109,6 +115,7 @@ var (
 
 	networkClient openstackclient.Networking
 	computeClient openstackclient.Compute
+	testId        = string(uuid.NewUUID())
 )
 
 var _ = BeforeSuite(func() {
@@ -146,14 +153,33 @@ var _ = BeforeSuite(func() {
 		},
 	}
 
-	cfg, err := testEnv.Start()
+	restConfig, err := testEnv.Start()
+	Expect(err).ToNot(HaveOccurred())
+	Expect(restConfig).ToNot(BeNil())
+
+	httpClient, err := rest.HTTPClientFor(restConfig)
 	Expect(err).NotTo(HaveOccurred())
-	Expect(cfg).NotTo(BeNil())
+	mapper, err := apiutil.NewDynamicRESTMapper(restConfig, httpClient)
+	Expect(err).NotTo(HaveOccurred())
+
+	scheme := runtime.NewScheme()
+	Expect(schemev1.AddToScheme(scheme)).To(Succeed())
+	Expect(extensionsv1alpha1.AddToScheme(scheme)).To(Succeed())
+	Expect(openstackinstall.AddToScheme(scheme)).To(Succeed())
 
 	By("setup manager")
-	mgr, err := manager.New(cfg, manager.Options{
+	mgr, err := manager.New(restConfig, manager.Options{
+		Scheme: scheme,
 		Metrics: metricsserver.Options{
 			BindAddress: "0",
+		},
+		Cache: cache.Options{
+			Mapper: mapper,
+			ByObject: map[client.Object]cache.ByObject{
+				&extensionsv1alpha1.Infrastructure{}: {
+					Label: labels.SelectorFromSet(labels.Set{"test-id": testId}),
+				},
+			},
 		},
 	})
 	Expect(err).NotTo(HaveOccurred())
@@ -562,6 +588,9 @@ func newInfrastructure(namespace string, providerConfig *openstackv1alpha1.Infra
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "infrastructure",
 			Namespace: namespace,
+			Labels: map[string]string{
+				"test-id": testId,
+			},
 		},
 		Spec: extensionsv1alpha1.InfrastructureSpec{
 			DefaultSpec: extensionsv1alpha1.DefaultSpec{

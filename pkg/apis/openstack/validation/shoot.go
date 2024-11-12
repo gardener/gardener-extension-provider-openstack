77b587823f95e65c93bebce35e7869eb0fa67b8f
Johannes Scheerer <johannes.scheerer@sap.com>
2024-11-12 14:46:34 +0100

    Enforce restriction with regards to IPv6 support (#886)

--- pkg/apis/openstack/validation/shoot.go
+++ pkg/apis/openstack/validation/shoot.go
@@ -30,6 +30,14 @@ func ValidateNetworking(networking *core.Networking, fldPath *field.Path) field.
 		allErrs = append(allErrs, field.Required(fldPath.Child("nodes"), "a nodes CIDR must be provided for Openstack shoots"))
 	}
 
+	if core.IsIPv6SingleStack(networking.IPFamilies) {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("ipFamilies"), networking.IPFamilies, "IPv6 single-stack networking is not supported"))
+	}
+
+	if len(networking.IPFamilies) > 1 {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("ipFamilies"), networking.IPFamilies, "dual-stack networking is not supported"))
+	}
+
 	return allErrs
 }
 

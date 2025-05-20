8bf796c9bc8de134852ad18912d1399bc96d3f79
georgibaltiev <georgi.baltiev@sap.com>
2025-05-20 13:02:27 +0300

    Update Controller Runnable function in accordance with the following discussion - https://github.com/gardener/gardener-extension-provider-aws/pull/1315

--- cmd/gardener-extension-provider-openstack/app/app.go
+++ cmd/gardener-extension-provider-openstack/app/app.go
@@ -237,12 +237,14 @@ func NewControllerManagerCommand(ctx context.Context) *cobra.Command {
 				return fmt.Errorf("could not add ready check for webhook server to manager: %w", err)
 			}
 
-			// TODO (georgibaltiev): remove after the release of version 1.49.0
-			log.Info("Adding migration runnables")
-			if err := mgr.Add(manager.RunnableFunc(func(ctx context.Context) error {
-				return purgeMachineControllerManagerRBACResources(ctx, mgr.GetClient())
-			})); err != nil {
-				return fmt.Errorf("error adding migrations: %w", err)
+			// TODO (georgibaltiev): Remove after the release of version 1.51.0
+			if reconcileOpts.ExtensionClass != "garden" {
+				log.Info("Adding migration runnables")
+				if err := mgr.Add(manager.RunnableFunc(func(ctx context.Context) error {
+					return purgeMachineControllerManagerRBACResources(ctx, mgr.GetClient(), log)
+				})); err != nil {
+					return fmt.Errorf("error adding migrations: %w", err)
+				}
 			}
 
 			if err := mgr.Start(ctx); err != nil {
--- cmd/gardener-extension-provider-openstack/app/migrations.go
+++ cmd/gardener-extension-provider-openstack/app/migrations.go
@@ -9,45 +9,58 @@ import (
 	"fmt"
 	"regexp"
 
+	kutil "github.com/gardener/gardener/pkg/utils/kubernetes"
+	"github.com/go-logr/logr"
 	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// TODO (georgibaltiev): Remove after the release of version 1.49.0
-func purgeMachineControllerManagerRBACResources(ctx context.Context, client client.Client) error {
+var nameRegex = regexp.MustCompile("extensions.gardener.cloud:provider-openstack:shoot--.*:machine-controller-manager")
+
+// TODO (georgibaltiev): Remove after the release of version 1.51.0
+func purgeMachineControllerManagerRBACResources(ctx context.Context, c client.Client, log logr.Logger) error {
+	log.Info("Starting the deletion of obsolete ClusterRoles and ClusterRoleBindings")
+
 	var (
 		clusterRoleBindingList = &rbacv1.ClusterRoleBindingList{}
 		clusterRoleList        = &rbacv1.ClusterRoleList{}
-		nameRegex              *regexp.Regexp
 	)
 
-	nameRegex, err := regexp.Compile("extensions.gardener.cloud:provider-openstack:shoot--.*:machine-controller-manager")
-	if err != nil {
-		return fmt.Errorf("failed to compile regex: %w", err)
-	}
-
-	if err := client.List(ctx, clusterRoleBindingList); err != nil {
-		return fmt.Errorf("failed to list clusterRoleBindings: %w", err)
+	if err := c.List(ctx, clusterRoleBindingList); err != nil {
+		return fmt.Errorf("failed to list ClusterRoleBindings: %w", err)
 	}
 
 	for _, clusterRoleBinding := range clusterRoleBindingList.Items {
 		if nameRegex.Match([]byte(clusterRoleBinding.Name)) {
-			if err := client.Delete(ctx, clusterRoleBinding.DeepCopy()); err != nil {
-				return fmt.Errorf("failed to delete clusterRoleBinding: %w", err)
+			log.Info("Deleting ClusterRoleBinding", "clusterRoleBinding", client.ObjectKeyFromObject(&clusterRoleBinding))
+			if err := kutil.DeleteObject(
+				ctx,
+				c,
+				&rbacv1.ClusterRoleBinding{ObjectMeta: metav1.ObjectMeta{Name: clusterRoleBinding.Name}},
+			); err != nil {
+				return fmt.Errorf("failed to delete ClusterRoleBinding %s: %w", client.ObjectKeyFromObject(&clusterRoleBinding), err)
 			}
 		}
 	}
 
-	if err := client.List(ctx, clusterRoleList); err != nil {
-		return fmt.Errorf("failed to list clusterRoles: %w", err)
+	if err := c.List(ctx, clusterRoleList); err != nil {
+		return fmt.Errorf("failed to list ClusterRoles: %w", err)
 	}
 
 	for _, clusterRole := range clusterRoleList.Items {
 		if nameRegex.Match([]byte(clusterRole.Name)) {
-			if err := client.Delete(ctx, clusterRole.DeepCopy()); err != nil {
-				return fmt.Errorf("failed to delete clusterRole: %w", err)
+			log.Info("Deleting ClusterRole", "clusterRole", client.ObjectKeyFromObject(&clusterRole))
+			if err := kutil.DeleteObject(
+				ctx,
+				c,
+				&rbacv1.ClusterRole{ObjectMeta: metav1.ObjectMeta{Name: clusterRole.Name}},
+			); err != nil {
+				return fmt.Errorf("failed to delete ClusterRole %s: %w", client.ObjectKeyFromObject(&clusterRole), err)
 			}
 		}
 	}
+
+	log.Info("Successfully deleted the obsolete ClusterRoles and ClusterRoleBindings")
 	return nil
 }

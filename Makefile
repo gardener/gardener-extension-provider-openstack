d41a9730e506e3307bea798a9b50b194df8b9e34
Tim Usner <tim.usner@sap.com>
2025-09-05 13:16:03 +0200

    Add generation for `Extension` example (#1136)

--- Makefile
+++ Makefile
@@ -168,7 +168,7 @@ check: $(GOIMPORTS) $(GOLANGCI_LINT)
 	@REPO_ROOT=$(REPO_ROOT) bash $(GARDENER_HACK_DIR)/check-charts.sh ./charts
 
 .PHONY: generate
-generate: $(VGOPATH) $(CONTROLLER_GEN) $(GEN_CRD_API_REFERENCE_DOCS) $(HELM) $(MOCKGEN) $(YQ)
+generate: $(VGOPATH) $(CONTROLLER_GEN) $(EXTENSION_GEN) $(GEN_CRD_API_REFERENCE_DOCS) $(HELM) $(KUSTOMIZE) $(MOCKGEN) $(YQ)
 	@REPO_ROOT=$(REPO_ROOT) VGOPATH=$(VGOPATH) GARDENER_HACK_DIR=$(GARDENER_HACK_DIR) bash $(GARDENER_HACK_DIR)/generate-sequential.sh ./charts/... ./cmd/... ./example/... ./pkg/...
 	$(MAKE) format
 
--- example/doc.go
+++ example/doc.go
@@ -3,6 +3,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //go:generate sh -c "bash $GARDENER_HACK_DIR/generate-crds.sh -p 20-crd- extensions.gardener.cloud resources.gardener.cloud"
+//go:generate sh -c "$TOOLS_BIN_DIR/extension-generator --name=provider-openstack --provider-type=openstack --component-category=provider-extension --extension-oci-repository=europe-docker.pkg.dev/gardener-project/public/charts/gardener/extensions/provider-openstack:$(cat ../VERSION) --admission-runtime-oci-repository=europe-docker.pkg.dev/gardener-project/public/charts/gardener/extensions/admission-openstack-runtime:$(cat ../VERSION) --admission-application-oci-repository=europe-docker.pkg.dev/gardener-project/public/charts/gardener/extensions/admission-openstack-application:$(cat ../VERSION) --destination=./extension/base/extension.yaml"
+//go:generate sh -c "$TOOLS_BIN_DIR/kustomize build ./extension -o ./extension.yaml"
 
 // Package example contains generated manifests for all CRDs and other examples.
 // Useful for development purposes.
